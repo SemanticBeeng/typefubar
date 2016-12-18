9be9eeaef5cd8fd569af505d17ee600da451db92
Karl Roberts <karl.roberts@owtelse.com>
2016-12-19 10:11:34 +1100

    example working: needs the Aux to come into play implicitly and I can get nicer signatures

---
+++ src/main/scala/fubar/Dummy.scala
@@ -0,0 +1,157 @@
+package fubar
+
+import fubar.MyTypeClass.Aux
+
+import scala.annotation.tailrec
+
+trait MyTypeClass[T] {
+  type In
+  type Out
+
+  def doMagic(foo: T, param1: In): Out
+}
+
+object MyTypeClass {
+
+  //aux pattern
+  type Aux[T0, In0, Out0] = MyTypeClass[T0] {type In = In0; type Out = Out0}
+
+  // convenience constructor so I don't need to use implicitly
+  def apply[T](implicit evidence: MyTypeClass[T]): Aux[T, evidence.In, evidence.Out] = evidence
+
+  // known type class members
+  implicit val greeterToTypeclass: Aux[Greeter, String, String] = new MyTypeClass[Greeter] {
+    type In = String
+    type Out = String
+
+    override def doMagic(foo: Greeter, param1: In): Out = foo.greeting(param1)
+  }
+
+  implicit val imAnAlienToTypeclass: Aux[ImAnAlien.type, Int, String] = new MyTypeClass[ImAnAlien.type] {
+    type In = Int
+    type Out = String
+
+    override def doMagic(foo: ImAnAlien.type, param1: In): Out = foo.say(param1)
+  }
+
+}
+
+case class Greeter(name: String) {
+
+  def greeting(greeting: String): String = {
+    s"$greeting, $name"
+  }
+}
+
+case object ImAnAlien {
+
+  @tailrec
+  def sayThis(thing: String, accumlator: String, numTimes: Int): String = numTimes match {
+    //base case
+    case 0 => accumlator
+    //recurse
+    case _ => sayThis(thing, accumlator + " " + thing, (numTimes - 1))
+
+  }
+
+  def say(num: Int): String = {
+    sayThis("I'm an Alien", "", num)
+  }
+}
+
+object SomeApi {
+  def voodoo[T, In0, Out0](foo: T, sayThis: In0)(implicit ev: MyTypeClass.Aux[T, In0, Out0]): Out0 = {
+    val tc = MyTypeClass[T]
+    tc.doMagic(foo, sayThis)
+    ev.doMagic(foo, sayThis)
+  }
+
+  def hoodoo[T, In0, Out0](foo: T, sayThis: In0)(implicit ev: MyTypeClass.Aux[T, In0, Out0]): Out0 = {
+    voodoo(foo, sayThis);
+  }
+}
+
+case class Yoodoo[T, In0, Out0](foo: T, sayThis: In0)(implicit ev: MyTypeClass.Aux[T, In0, Out0]) {
+
+
+  import SomeApi._
+
+  def doVoodoo(): Out0 = {
+    voodoo(foo, sayThis);
+//    ev.doMagic(foo, sayThis)
+  }
+}
+
+class Fakir[T,In0,Out0](foo: T, sayThis: In0)(implicit ev: MyTypeClass.Aux[T,In0,Out0]) {
+
+  import SomeApi._
+
+  def doVoodoo(): Out0 = {
+    voodoo(foo, sayThis);
+//    ev.doMagic(foo, sayThis);
+  }
+}
+
+object Fakir {
+  def apply[T, In0, Out0](foo: T, sayThis: In0)(implicit ev: Aux[T, In0, Out0]) = new Fakir(foo,sayThis)
+}
+
+
+class Charleton[T,In0, Out0]()
+
+object Main extends App {
+
+  //bring typeclass instance into scope
+  import MyTypeClass._
+  import SomeApi._
+
+  println("Greeting:- ")
+  println(voodoo(Greeter("karl"), "wassup"))
+
+  println("\nAliens:- ")
+  println(voodoo(ImAnAlien, 3))
+
+
+}
+
+object Main1 extends App {
+
+  //bring typeclass instance into scope
+  import MyTypeClass._
+  import SomeApi._
+
+  println("Greeting hoodoo:- ")
+  println(hoodoo(Greeter("hoodo karl"), "wassup"))
+
+  println("\nAliens hoodoo :- ")
+  println(voodoo(ImAnAlien, 3))
+
+}
+
+object Main2 extends App {
+
+  //bring typeclass instance into scope
+  import MyTypeClass._
+  import SomeApi._
+
+  val yoodoo = Yoodoo(Greeter("YOODOO karl"), "Howzithangin")
+  println("Greeting yoodoo:- ")
+  println(yoodoo.doVoodoo())
+
+
+}
+
+object Main3 extends App {
+
+  //bring typeclass instance into scope
+  import MyTypeClass._
+  import SomeApi._
+
+  val fakir = Fakir(Greeter("YOODOO karl"), "Howzithangin")
+  println("Greeting yoodoo:- ")
+  println(fakir.doVoodoo())
+
+
+}
+
+
